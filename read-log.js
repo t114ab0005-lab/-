d3ed31e67984d7cac3b6036276a0d84787975651
lintree666 <oppo6linricky@gmail.com>
2026-06-15 14:50:03 +0800

    Delete read-log.js

--- read-log.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-try {
-  const content = fs.readFileSync('test-run.log', 'utf8');
-  fs.writeFileSync('log-output.js', '// ' + content.replace(/\r?\n/g, '\n// '));
-  console.log("Log read successfully");
-} catch (e) {
-  fs.writeFileSync('log-output.js', '// Error: ' + e.message);
-}
-process.exit(0);
