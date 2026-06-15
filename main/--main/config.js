dd3f20199671bfd89cf7418d256d54fc7eecb734
t114ab0005-lab <t114ab0005@ntut.org.tw>
2026-06-15 14:40:16 +0800

    Delete main/--main directory

--- main/--main/config.js
+++
@@ -1,1 +0,0 @@
-const GEMINI_API_KEY = "[ENCRYPTION_KEY]";
--- main/--main/copy-log.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-try {
-  fs.copyFileSync('test-run.log', 'test-run-out.js');
-  console.log("Successfully copied!");
-} catch (e) {
-  console.error("Copy failed:", e);
-}
-process.exit(0);
